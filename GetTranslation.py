6f47ff601480e4af90caed76364de2d707e934b3
Bean-jun <1342104001@qq.com>
2020-12-16 11:22:59 +0800

    修复GetTranslation中数据获取漏洞，ContentToDatabase支持添加mysql支持

--- GetTranslation.py
+++ GetTranslation.py
@@ -10,12 +10,14 @@ from ContentToDatabase import DatabaseOperate
 
 def get_translation(*args):
     DATE_BASE = './WordLearn.db'
-    db = DatabaseOperate(DATE_BASE)
+    db = DatabaseOperate(DATE_BASE, )
     for word in args:
         tag = random.uniform(0, 2)
         try:
             word_to_num = ord(word[0])
-            flag = tag <= 0.5 and 65 < word_to_num < 122
+
+            # 使用不同爬虫的限制,False则为YouDaoTranslate爬虫,True则BaiDuTranslate为爬虫
+            flag = tag >= 0.5 and 65 < word_to_num < 122
 
             # 判断当前单词是否存在数据库中,get_download_flag为False则表明不存在
             get_download_flag = bool(list(get_result("""select word from word_table where word='{}'""".format(word))))
@@ -24,7 +26,7 @@ def get_translation(*args):
 
                 FILE_PATH = "{}/{}.mpeg".format('./Media', urllib.parse.quote(word))
 
-                if flag is True:
+                if flag is False:
                     time.sleep(1)
                     translate = YouDaoTranslate(word).get_translate_content()
                     db.insert('word_table', word=word, word_translate=translate, word_sound=FILE_PATH)
@@ -41,5 +43,5 @@ def get_translation(*args):
     
 if __name__ == "__main__":
     t1 = time.time()
-    get_translation('exception', 'content', 'constraint', 'invalid', 'plugin', 'ten', 'readme')
+    get_translation('补丁', 'content', 'constraint', 'invalid', 'plugin', 'ten', 'readme')
     print(time.time()-t1)
