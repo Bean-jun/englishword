167f4d83bea18f0017ee746e73608f6a87bcca68
Bean-jun <1342104001@qq.com>
2020-12-24 10:35:47 +0800

    添加路由功能

--- frame/mini_frame.py
+++ frame/mini_frame.py
@@ -1,3 +1,19 @@
+# 配置路由
+URL_CONTENT_DICT = dict()
+
+
+def route(string):
+    '''
+    创建路由功能
+    :param string: 页面地址
+    :return: None
+    '''
+    def call_func(function):
+        URL_CONTENT_DICT[string] = function
+    return call_func
+
+
+@route('/index.mini')
 def index():
     with open("templates/index.html", encoding='utf-8') as f:
         return f.read()
@@ -6,9 +22,7 @@ def index():
 def application(env, set_header):
     set_header('200 OK', [("Content-Type", "text/html;charset=utf-8")])
     file_name = env['PATH_INFO']
-
-    if file_name == "/index.mini":
-        print('this is frame')
-        return index()
-    else:
-        return "___mini___frame__404_____"
+    try:
+        return URL_CONTENT_DICT[file_name]()
+    except Exception as e:
+        return "content error:{}".format(e.args)
